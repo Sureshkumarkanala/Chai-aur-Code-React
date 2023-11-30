cc32d68926e4717e986f2661161fb8dfece2c489
Sureshkumarkanala <kanalasureshkumar@gmail.com>
2023-12-01 00:02:43 +0530

    Props child to parent

--- basicprojects/Onlyprops/src/App.jsx
+++ basicprojects/Onlyprops/src/App.jsx
@@ -1,61 +1,83 @@
-import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-import "./App.css";
+// import { useState } from "react";
+
+// import Props from "./Props";
+// // let value = 999;
+// // const myarr = [1, 2, 3];
+// // const myObj = {
+// //   name: "Arun",
+// //   age: 25,
+// //   company: "HCL",
+// // };
+// // const myobjone = {
+// //   name: "Suresh",
+// //   age: 25,
+// //   company: "HCl",
+// // };
+// // function callfunction() {
+// //   alert("Function is passed as a prop");
+// // }
+
+// // function calldata(num1, num2) {
+// //   console.log(num1 * num2);
+// //   alert("Add button clicked");
+// // }
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [inval, setInval] = useState(1);
+
+//   function statechange(a) {
+//     setCount(a * 10);
+//   }
+
+//   const valuechange = (num) => {
+//     console.log("num is ", num);
+//     setInval(num * 100);
+//   };
+//   return (
+//     <>
+//       {/* <Props name="Arun" num={value} /> */}
+//       {/* <Props name={myObj.name} age={myObj.age} company={myObj.company} /> */}
+//       {/* <Props {...myobjone} /> */}
+//       {/* <Props arr1={[10, 20, 30]} /> */}
+//       {/* <Props {...[myarr]} /> */}
+//       {/* <Props data={callfunction()} /> */}
+
+//       <p>state initial value is {count}</p>
+//       {/* <Props addfun={calldata} /> */}
+//       <Props statevalue={statechange} />
+//       <p>First value is {inval}</p>
+//       <Props inpdata={valuechange} />
+//     </>
+//   );
+// }
+
+// export default App;
+
+// 1. Passing name="Arun" num={value}  as a this.props
+// 2.passing array as a props(By using destucture)
+//3.passing the object individually as a prop
+//4.pass the totalobject as prop(in child componnet we have to use the same names while destucture)
+// 5. pass array along with values inside the prop we receive total array as a prop we need to use join
+//  to display by saperated by ',' data.arr1.join()
+// 6.send array as a prop using destructure in child we use by ushing join      <p>{data[0].join()}</p>
+//7.execute the function inside the prop
+//8.pass the function written in the parent,and pass  as a prop to the child component
+//  and give the arguements in the child it will execute in the parent
+
+import React, { useState } from "react";
 import Props from "./Props";
-// let value = 1000;
-
-const myarr = [1, 2, 3];
-
-const myObj = {
-  name: "Arun",
-  age: 30,
-  company: "HCL",
-};
-const myobjone = {
-  name: "Suresh",
-  age: 30,
-  company: "HCl",
-};
-
-// function callfunction() {
-//   alert("Function is passed as a prop");
-// }
-
-// function calldata(num1, num2) {
-//   alert("Add butto cicked");
-
-//   return console.log(num1 * num2);
-// }
-
 function App() {
-  // const [count, setCount] = useState(0);
-  const [inval, setInval] = useState(1);
+  const [firstalue, setFirstvalue] = useState(0);
 
-  // function statechange(a) {
-  //   setCount(a * 10);
-  // }
-
-  const valuechange = (num) => {
-    console.log(num);
-    setInval(num * 100);
+  const multiply = (val) => {
+    setFirstvalue(val * 10);
   };
   return (
-    <>
-      {/* <Props name="Arun" num={value}  /> */}
-      {/* <Props name={myObj.name} age={myObj.age} company={myObj.company} /> */}
-      {/* <Props {...myobjone} /> */}
-      {/* <Props arr1={[10, 20, 30]} /> */}
-
-      {/* <Props {...[myarr]} /> */}
-
-      {/* <Props data={callfunction} /> */}
-      {/* <p>state initial value is {count}</p> */}
-      {/* <Props data={calldata} /> */}
-      {/* <Props statevalue={statechange} /> */}
-      <p>First value is {inval}</p>
-      <Props inpdata={valuechange} />
-    </>
+    <div>
+      <p>Initial value is {firstalue}</p>
+      <Props changefuntion={multiply} />
+    </div>
   );
 }
 
--- basicprojects/Onlyprops/src/Props.jsx
+++ basicprojects/Onlyprops/src/Props.jsx
@@ -1,31 +1,68 @@
-import { React } from "react";
-// function Props({ name, age, company, data }) {
+// import { React } from "react";
+// // function Props({ name, age, company }) {
 // function Props(data) {
+//   // console.log(data.data);
+//   // console.log("props as", data.arr1[0]);
+//   // console.log(data.arr1);
+//   // function Props(arr) {
+//   // { name, num }
+//   //   const { name, age, company } = data;
+//   //Sending an arary as props
+//   // console.log("arr is ", arr);
+//   //  console.log(arr[0][2]);
+//   //   const arr1 = data;
+//   //   console.log(arr1);
+//   return (
+//     <div>
+//       Props Component
+//       <hr />
+//       {/* <p>
+//         {name}
+//         {num}
+//       </p> */}
+//       {/* <p>{num}</p> */}
+//       {/* <p>{name}</p>
+//       <p>{age}</p>
+//       <p>{company}</p> */}
+//       {/* <div>
+//         {name}
+//         {age}
+//         {company}
+//       </div> */}
+//       {/* <p>Array is {data.arr1.join()}</p> */}
+//       {/* <p>{data[0].join()}</p> */}
+//       {/* <p>{data}</p> */}
+//       {/* <p>{arr.map((val) => val * 2)}</p> */}
+//       {/* <button onClick={data}>Click </button> */}
+//       {/* <button onClick={(e) => data.addfun(10, 10)}>Add </button> */}
+//       <button onClick={(e) => data.statevalue(10)}>State value</button>
+//       <button
+//         onClick={(e) => {
+//           data.inpdata(2);
+//         }}
+//       >
+//         click
+//       </button>
+//       {/* <button onClick={(e) => data.inpdata(10)}>Click me</button> */}
+//     </div>
+//   );
+// }
+// export default Props;
+
+import React from "react";
+
 function Props(data) {
-  //   const { name, age, company } = data;
-  //   const arr = data[0];
-  //   console.log(arr[0][1]);
-
-  //   const arr1 = data;
-  //   console.log(arr1);
-
   return (
     <div>
-      Props Component
-      <hr />
-      {/* <p>{name}</p>
-      <p>{num}</p> */}
-      {/* <p>{name}</p>
-      <p>{age}</p>
-      <p>{company}</p> */}
-      {/* <p>Array is {data.arr1.join("")}</p> */}
-      {/* <p>{arr.join("")}</p> */}
-      {/* <p>{arr.map((val) => val * 2)}</p> */}
-      {/* <button onClick={data.data}>Click </button> */}
-      {/* <button onClick={(e) => data.data(10, 10)}>Add </button> */}
-      {/* <button onClick={(e) => data.statevalue(10)}>State value</button> */}
-      <button onClick={(e) => data.inpdata(100)}>Click me</button>
+      <button
+        onClick={() => {
+          data.changefuntion(5);
+        }}
+      >
+        multiply{" "}
+      </button>
     </div>
   );
 }
+
 export default Props;
